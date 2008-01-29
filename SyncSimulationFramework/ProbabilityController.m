574cc2b5a4676ca89e6aa67dedaac5b51f5e3808
Brad BARCLAY <yaztromo@gmail.com>
2008-01-29 20:40:18 +0000

    Added license and copyright information to every source file.
    git-svn-id: https://svn.code.sf.net/p/tricklesync/code/trunk@3 dd4b9e15-e244-0410-b754-811dee3e6019

--- SyncSimulationFramework/ProbabilityController.m
+++ SyncSimulationFramework/ProbabilityController.m
@@ -1,10 +1,26 @@
+// --------------------------------------------------------------------------
+// The Expedient Trickle Sync Project -- Source File.
+// Copyright (c) 2008 Brad BARCLAY <bbarclay@jsyncmanager.org>
+// --------------------------------------------------------------------------
+// OSI Certified Open Source Software
+// --------------------------------------------------------------------------
 //
-//  ProbabilityController.m
-//  SyncSimulationFramework
+// This program is free software; you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by the Free 
+// Software Foundation; either version 2 of the License, or (at your option) 
+// any later version.
 //
-//  Created by Brad Barclay on 2007/12/11.
-//  Copyright 2007 __MyCompanyName__. All rights reserved.
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
+// more details.
 //
+// You should have received a copy of the GNU General Public License along 
+// with this program; if not, write to the Free Software Foundation, Inc., 
+// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+// --------------------------------------------------------------------------
+
 
 #import "ProbabilityController.h"
 
