c8b21103e813d64546b4691eb57c12be1391d6f0
Brad BARCLAY <yaztromo@gmail.com>
2008-02-07 03:54:15 +0000

    Added the Xcode project for the main simulator GUI executable.
    git-svn-id: https://svn.code.sf.net/p/tricklesync/code/trunk@25 dd4b9e15-e244-0410-b754-811dee3e6019

---
+++ SyncSimulator/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SyncSimulator
+//
+//  Created by Brad Barclay on 2008/02/06.
+//  Copyright __MyCompanyName__ 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
